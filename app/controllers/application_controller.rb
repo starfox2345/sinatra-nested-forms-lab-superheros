d5bd05168cf0011ff7b643e799a75cadfc084c09
yatsun <yatsunlam25@gmail.com>
2021-05-27 15:07:44 -0400

    commit tree bbda1c1299f69181d7e581df65d5b5ef732d19a9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,21 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+
+        erb :super_hero
+    end
+
+    post '/teams' do
+        @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+        members = params[:team][:members]
+
+        members.each do |member_parameter|
+            SuperHero.new({name: member_parameter[:name], power: member_parameter[:power], bio: member_parameter[:bio]})
+        end
+        @super_heroes = SuperHero.all
+        
+        erb :team
+    end
 
 end
---
+++ app/models/super_hero.rb
@@ -0,0 +1,20 @@
+class SuperHero
+    attr_accessor :name, :power, :bio
+    @@all = []
+
+    def initialize(params)
+        @name = params[:name]
+        @power = params[:power]
+        @bio = params[:bio]
+
+        self.save
+    end
+
+    def save
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
